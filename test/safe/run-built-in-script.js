763d251736879cfd25b4a3caacbb8316518b7908
Justin Searls <searls@gmail.com>
2016-04-06 13:22:02 -0400

    Add a second integration test for the error case.

--- test/safe/run-built-in-script.js
+++ test/safe/run-built-in-script.js
@@ -4,12 +4,15 @@ var intercept = require('intercept-stdout')
 var assert = require('assert')
 var _ = require('lodash')
 
-var stdout, stopIntercept
+var stdout, stderr, stopIntercept
 module.exports = {
   beforeEach: function () {
     stdout = ''
+    stderr = ''
     stopIntercept = intercept(function (text) {
       stdout += text
+    }, function (text) {
+      stderr += text
     })
   },
   outputAndRunScript: function () {
@@ -23,6 +26,13 @@ module.exports = {
 
     assert(_.includes(stdout, '> echo Hello, $WORLD!'), 'prints script')
     assert(_.includes(stdout, 'Hello, World!'), 'prints output')
+  },
+  noScriptFound: function () {
+    scripty('fake:noscriptfound')
+
+    assert(_.includes(stderr,
+      'Error: scripty - no script found for npm lifecycle "fake:noscriptfound"'
+    ))
   },
   afterEach: function () {
     stopIntercept()
