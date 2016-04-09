92aa93057f7276facf5779f7e3208e46b972f365
Justin Searls <searls@gmail.com>
2016-04-09 13:59:17 -0400

    Start the process of supporting multi-execution

--- test/safe/dir-test.js
+++ test/safe/dir-test.js
@@ -7,5 +7,14 @@ module.exports = {
       assert.includes(stdio.stdout, 'rubby')
       done(er)
     })
+  },
+  runAllWhenDirHasNoIndex: function (done) {
+    runScripty('parent', {}, function (er, code, stdio) {
+      assert.equal(code, 0)
+      assert.includes(stdio.stdout, 'AAA')
+      assert.includes(stdio.stdout, 'BBB')
+      assert.includes(stdio.stdout, 'CCC')
+      done(er)
+    })
   }
 }
