2ad96902e6c9d6ca4b87071ca4c4dbbeac4d289f
Justin Searls <searls@gmail.com>
2016-04-09 22:24:12 -0400

    Use log inspection instead of td.replace
    Since td.replace doesn't work in Node 0.10 and
    this test is intended to give us regression feedback,
    remove it.

--- test/unit-helper.js
+++ test/unit-helper.js
@@ -1,8 +1,13 @@
 global.td = require('testdouble')
-global.assert = require('assert')
+
+var decorateAssertions = require('./decorate-assertions')
+global.assert = decorateAssertions(require('assert'))
+
+var log = require('../lib/log')
 
 module.exports = {
   afterEach: function () {
     td.reset()
+    log.reset()
   }
 }
