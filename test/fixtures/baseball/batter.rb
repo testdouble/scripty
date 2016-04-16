0ebc59b4a389a60f2c3a02f4236cb856422d8d15
Justin Searls <searls@gmail.com>
2016-04-16 00:52:23 -0400

    Make the test less bad.

--- test/fixtures/baseball/batter.rb
+++ test/fixtures/baseball/batter.rb
@@ -1,11 +1,11 @@
 def wait_for_file(name)
-  while !File.exist?(name)
+  while !File.exist?(File.join(File.dirname(__FILE__), name))
     sleep 0.1
   end
 end
 
 def write_file(name)
-  File.open(name, 'w') {|f| f.write('stuff') }
+  File.open(File.join(File.dirname(__FILE__), name), 'w') {|f| f.write('stuff') }
 end
 
 puts "Batter waits for ball"
--- test/safe/parallel-test.js
+++ test/safe/parallel-test.js
@@ -5,16 +5,10 @@ var _ = require('lodash')
 var runScripty = require('../run-scripty')
 
 module.exports = {
-  beforeEach: function () {
-    _(['baseball/bat', 'baseball/ball']).each(function (f) {
-      try {
-        fs.unlinkSync(path.resolve('test/fixtures/user-scripts/parallel', f))
-      } catch (e) {}
-    })
-  },
+  beforeEach: teardown,
+  afterEach: teardown,
   works: function (done) {
     runScripty('parallel', {parallel: true}, function (er, code, stdio) {
-      console.log(er, code, stdio)
       assert.equal(0, code)
       assert.includes(stdio.stdout, 'Batter waits for ball')
       assert.includes(stdio.stdout, 'Pitcher writes ball')
@@ -25,4 +19,12 @@ module.exports = {
       done(er)
     })
   }
+}
+
+function teardown () {
+  _(['bat', 'ball']).each(function (f) {
+    try {
+      fs.unlinkSync(path.resolve('test/fixtures/baseball', f))
+    } catch (e) {}
+  })
 }
