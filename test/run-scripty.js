2621b1ba05c6b6a40726a3692504dd6eaf39de53
Justin Searls <justin@testdouble.com>
2016-04-16 17:08:58 -0700

    Fix windows

--- test/run-scripty.js
+++ test/run-scripty.js
@@ -4,22 +4,15 @@ var path = require('path')
 var scripty = require('../lib/scripty')
 var grabStdio = require('./grab-stdio')
 
-var builtInPath, userPath
-if (process.platform === 'win32') {
-  builtInPath = path.resolve('test/fixtures/built-in-scripts-win')
-  userPath = path.resolve('test/fixtures/user-scripts-win')
-} else {
-  builtInPath = path.resolve('test/fixtures/built-in-scripts')
-  userPath = path.resolve('test/fixtures/user-scripts')
-}
-
 module.exports = function (name, opts, cb) {
   var stdio = {}
 
   scripty(name, _.defaultsDeep({}, opts, {
     resolve: {
-      builtIn: builtInPath,
-      scripts: userPath
+      builtIn: path.resolve('test/fixtures/built-in-scripts'),
+      builtInWin: path.resolve('test/fixtures/built-in-scripts-win'),
+      scripts: path.resolve('test/fixtures/user-scripts'),
+      scriptsWin: path.resolve('test/fixtures/user-scripts-win')
     },
     spawn: {
       tap: grabStdio(stdio)
