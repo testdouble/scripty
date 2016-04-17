4acf7fe54727a57b8f3585bf81ac044a3c0e9716
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2016-04-17 22:31:33 +0200

    allow dry run test to pass on windows

--- test/safe/dry-run.js
+++ test/safe/dry-run.js
@@ -1,10 +1,12 @@
-var runScripty = require('../run-scripty')
+var path = require('path')
 var _ = require('lodash')
+
+var runScripty = require('../run-scripty')
 var log = require('../../lib/log')
 
 module.exports = function doesNotRunButPrintScripts (done) {
   runScripty('hello:world', {dryRun: true}, function (er, code, stdio) {
-    assert.includes(log.read(), 'built-in-scripts/hello/world')
+    assert.includes(log.read(), 'built-in-scripts' + path.sep + 'hello' + path.sep + 'world')
     assert.ok(!_.includes(stdio.stdout, 'Hello, World!'), 'Stdout includes "Hello World!"')
     done(er)
   })
