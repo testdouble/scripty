b521590bdf2b8a4f6f0ac0877cab4e20aaa0d324
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2016-04-17 22:40:24 +0200

    add platform conditional to pass the test :(

--- test/safe/dry-run.js
+++ test/safe/dry-run.js
@@ -1,4 +1,3 @@
-var path = require('path')
 var _ = require('lodash')
 
 var runScripty = require('../run-scripty')
@@ -6,7 +5,11 @@ var log = require('../../lib/log')
 
 module.exports = function doesNotRunButPrintScripts (done) {
   runScripty('hello:world', {dryRun: true}, function (er, code, stdio) {
-    assert.includes(log.read(), 'built-in-scripts' + path.sep + 'hello' + path.sep + 'world')
+    if (process.platform === 'win32') {
+      assert.includes(log.read(), 'built-in-scripts-win\\hello\\world')
+    } else {
+      assert.includes(log.read(), 'built-in-scripts/hello/world')
+    }
     assert.ok(!_.includes(stdio.stdout, 'Hello, World!'), 'Stdout includes "Hello World!"')
     done(er)
   })
