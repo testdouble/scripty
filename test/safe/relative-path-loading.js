bf1ddf84c7ec17078b6451d14dd1bca15dd41d08
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2016-11-04 12:40:31 -0700

    Add test for loading custom script paths.

---
+++ test/safe/relative-path-loading.js
@@ -0,0 +1,27 @@
+var fork = require('child_process').fork
+var path = require('path')
+var grabStdio = require('../grab-stdio')
+
+module.exports = {
+  'loads scripts from a relative path': function (done) {
+    var stdio = {}
+    var child = fork('../../../cli', [], {
+      cwd: path.join(__dirname, '..', 'fixtures', 'relative-path-loading'),
+      silent: true,
+      env: {
+        npm_lifecycle_event: 'secret',
+        npm_package_scripty_path: '../custom-user-scripts',
+        SCRIPTY_SILENT: true
+      }
+    })
+
+    grabStdio(stdio)(child)
+
+    child.on('exit', function (code) {
+      assert.equal(code, 0)
+      assert.includes(stdio.stdout, 'SSHHH')
+
+      done()
+    })
+  }
+}
