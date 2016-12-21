d69a71fe0c54170042988dd4d5200e0ce2a8d3bb
Justin Searls <searls@gmail.com>
2016-12-21 10:14:06 -0500

    Fix relative path test on windows

--- test/safe/relative-path-loading.js
+++ test/safe/relative-path-loading.js
@@ -5,12 +5,13 @@ var grabStdio = require('../grab-stdio')
 module.exports = {
   'loads scripts from a relative path': function (done) {
     var stdio = {}
+    var windowsSuffix = process.platform === 'win32' ? '-win' : ''
     var child = fork('../../../cli', [], {
       cwd: path.join(__dirname, '..', 'fixtures', 'relative-path-loading'),
       silent: true,
       env: {
         npm_lifecycle_event: 'secret',
-        npm_package_scripty_path: '../custom-user-scripts',
+        npm_package_scripty_path: '../custom-user-scripts' + windowsSuffix,
         SCRIPTY_SILENT: true
       }
     })
