8f41ccaf527146c0aeeb3f31a9f45fc972c77eac
Jason Karns <jason@karns.name>
2018-12-18 12:13:37 -0500

    Normalize the log level when setting the level

--- test/safe/silent-test.js
+++ test/safe/silent-test.js
@@ -1,11 +1,10 @@
 var runScripty = require('../run-scripty')
-var log = require('../../lib/run/log')
 
 module.exports = function doesNotEchoScriptContentInSilentMode (done) {
   var oldConsole = global.console
   global.console = {} // blow up if a console method is invoked
 
-  runScripty('hello:world', { logLevel: log.levels.SILENT }, function (er, code, stdio) {
+  runScripty('hello:world', { logLevel: 'silent' }, function (er, code, stdio) {
     global.console = oldConsole
     done(er)
   })
