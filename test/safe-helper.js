a88be5eb3911dccf40698422600bffb71e558304
Jason Karns <jason@karns.name>
2018-12-19 10:09:22 -0500

    Moves log from lib/run/ to lib/
    The logger is needed before the run process begins so it's no longer a
    local dep of just the run process.
    
    (For instance, to write errors/warnings during the loading process.)

--- test/safe-helper.js
+++ test/safe-helper.js
@@ -1,7 +1,7 @@
 var decorateAssertions = require('./decorate-assertions')
 global.assert = decorateAssertions(require('assert'))
 
-var log = require('../lib/run/log')
+var log = require('../lib/log')
 
 module.exports = {
   afterEach: function () {
