f58782b95d5bc3a675d6965da9ebd4034f47c457
Jason Karns <jason@karns.name>
2018-12-21 10:52:04 -0500

    Silence log in safe test

--- test/safe/no-opts-spot-check.js
+++ test/safe/no-opts-spot-check.js
@@ -1,6 +1,8 @@
-var scripty = require('../../lib/scripty')
+const scripty = require('../../lib/scripty')
 
 module.exports = function worksWithNoOpts (done) {
+  process.env.npm_config_loglevel = 'silent'
+
   scripty('noop', function (er, code) {
     assert.equal(0, code)
     done(er)
